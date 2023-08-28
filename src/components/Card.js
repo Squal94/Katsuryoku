b939576b90a871ef02ca2f591eedf80ab57b4cce
squal94 <lounis.khadir@wanadoo.fr>
2023-08-28 11:58:46 +0200

    debut du composant card v2

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,11 @@ import React from "react";
 
 const Card = ({ data }) => {
   console.log(data);
-  return <div className="cardContainer"></div>;
+  return (
+    <div className="cardContainer">
+      <h1>test</h1>
+    </div>
+  );
 };
 
 export default Card;
