184b8b4f5f1f19ccc8e011ea9f442979955cfbed
squal94 <lounis.khadir@wanadoo.fr>
2023-08-29 10:49:54 +0200

    page about debut

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,24 @@
 import React from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const About = () => {
-  return <div></div>;
+  return (
+    <div className="aboutContainer">
+      <Header />
+      <main className="aboutContainer__main">
+        <div className="aboutContainer__main--bannerImg">
+          <img src="" alt="" />
+        </div>
+        <section className="aboutContainer__main__history">
+          <div className="aboutContainer__main__history--img"></div>
+          <div className="aboutContainer__main__history--content"></div>
+        </section>
+        <div className="aboutContainer__main__video"></div>
+      </main>
+      <Footer />
+    </div>
+  );
 };
 
 export default About;
